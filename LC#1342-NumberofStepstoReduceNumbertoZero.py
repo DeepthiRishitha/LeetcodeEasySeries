6111a50445bb7f9b2992e5f0b39f1426c4c51eec
Deepthi Rishitha <99371241+DeepthiRishitha@users.noreply.github.com>
2025-02-27 18:55:31 -0800

    Add files via upload

---
+++ LC#1342-NumberofStepstoReduceNumbertoZero.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def numberOfSteps(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        self.num = num
+        count = 0
+        while(num > 0):
+            if num % 2 == 0:
+                num /= 2
+                count += 1
+            else:
+                num -= 1 
+                count += 1
+        return count
