8c30d70e4f0dd6b508947353847404c1cfb128a0
Deepthi Rishitha <99371241+DeepthiRishitha@users.noreply.github.com>
2025-03-24 19:02:07 -0700

    Update LC#1832-CheckiftheSentenceIsPangram.py

--- LC#1832-CheckiftheSentenceIsPangram.py
+++ LC#1832-CheckiftheSentenceIsPangram.py
@@ -1,1 +1,12 @@
-
+class Solution(object):
+    def checkIfPangram(self, sentence):
+        """
+        :type sentence: str
+        :rtype: bool
+        """
+        self.sentence = sentence
+        unique_chars = set(sentence)
+        if len(unique_chars) == 26:
+            return True
+        else:
+            return False
